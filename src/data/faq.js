21cd879bbb3b54a1b6d0683c855e56ba066c9515
Jhonattas Henrique <jhonattas@soucriador.com>
2017-06-22 09:49:33 -0300

    termina o arquivo de faq

--- src/data/faq.js
+++ src/data/faq.js
@@ -9,68 +9,68 @@ module.exports =
   	content: 'Aceitamos cartões e boletos, tudo feito pela PagSeguro ou Paypal para manter em segurança todos os dados.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Posso cancelar a qualquer momento?'
+  	content: 'Sim, pode! Não fazemos contrato de permanência conosco!'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Posso utilizar uma conta nova?'
+  	content: 'Não. Sua conta deve ter no mínimo 1 mês, quanto mais nova a conta menos interações por dia são possíveis. O ideal são contas com mais de 6 meses, ao menos 15 posts e 500 seguidores. Porém assim que rodando no nosso aumentamos as interações a cada semana, alcançando o máximo possível em menos de 2 meses.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'O número de pessoas seguidas vai aumentar?'
+  	content: 'Naturalmente, desde que para conseguirmos um resultado melhor não podemos deixar de seguir as pessoas que nos seguem de volta tão rapidamente ou existe a chance deles nos deixar de seguir é muito maior. Porém deixamos de seguir as pessoas que não nos seguirem de volta, o que faz com que o número de pessoas que seguimos aumente de acordo com o número de seguidores que ganhamos.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Posso acessar minha conta normalmente?'
+  	content: 'Você pode acessar sua conta normalmente para fazer postagens, responder mensagens e comentários, porém jamais faça uso do famoso “spam”, que é o ato de mandar a mesma mensagem para vários usuários via DM ou comentário. isso não é recomendado nem para quem não é nosso assinante, o Instagram tem as punições mais severas para esse tipo de mensagem.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'É possivel apenas curtir para ser seguido?'
+  	content: 'Sim, é possível. Porém tal método é muito menos efetivo que a combinação de curtir e seguir seu público alvo. Somente contas com uma administração muito boa, com fotos igualmente excelentes ou então algum ramo ou produto em que o Instagram seja uma muito bom tal método funcionaria, porém nós não recomendamos e até preferimos não atender.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Vocês vão gerenciar minha conta?'
+  	content: 'Não.<br/><br/> Nosso serviço baseia-se somente em interações, não fazemos nenhum tipo de criação ou gerenciamento. Nossa ferramenta segue e curti perfis relacionados ao seu ramo ou localização e depois deixa de seguir aqueles que não nos seguirem de volta, nada mais.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Vocês precisam da minha senha?'
+  	content: 'Sim. Não existe a possibilidade de se seguir, curtir ou deixar de seguir perfis sem acesso a conta. <br/>Somos uma empresa séria e mantemos seus dados em máxima segurança!'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'As contas que irão me seguir são reais?'
+  	content: 'Sim!<br/<br/>Não somos como aqueles sites que vendem seguidores falsos para apenas alavancar a popularidade da sua conta, nós conseguimos seguidores reais interagindo com eles de forma automática.<br/<br/>Nossa ferramenta segue e curte suas fotos, e muitos curtem e seguem de volta essas contas. Além de serem reais é possível atingir seus potenciais clientes seguindo pessoas que seguem contas de competidores ou atrações locais.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Vocês garantem a entrega dos seguidores?'
+  	content: 'Sim! Garantimos 1.000 seguidores por mês, ou devolvemos seu dinheiro.<br/<br>Nós não garantimos os seguidores nos seguintes casos:<br/><br/>Sua conta tem menos de 3 meses;<br/>Sua conta possui menos que 500 seguidores;<br/>A qualidade das postagens é muito baixa;<br/>Você faz menos que 5 postagens por semana;<br/>Seu ramo é voltado para Pessoa Jurídica;<br/>Sua conta possui seguidores falsos.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Vou ganhar 5000 seguidores por mês?'
+  	content: 'É possível, porém não é uma tarefa das mais fáceis.<br/><br/><br/>Como nosso serviço é baseado apenas em interações com seus usuários não depende só de nós para conseguirmos um bom desempenho.<br/><br/><br/>Outro fator é muito importante, o ramo de negócio. Tatuadores, barbeiros, personal trainers, lojas de roupa e similares vão muito bem no nosso serviço e esses ramos com uma ótima administração e um trabalho bem feito nas postagens conseguirá com certeza números acima da média 3.000 seguidores mensais.<br/><br/>Para contas desses ramos citados e com uma boa administração garantimos no mínimo 1.000 seguidores novos reais por mês ou devolvemos seu investimento. É mais barato do que alguns vendedores que vendem seguidores falsos.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Como escolho os perfis a serem seguidos?'
+  	content: 'Existem 2 métodos para conseguirmos atingir o seu público alvo.<br/>Primeiro por localização, conseguimos focar em pessoas em uma determinada região a nível de cidade e estado. <br/>Segundo por hashtags que os usuários usaram. Nossa ferramenta consegue atingir pessoas que usaram determinada hashtag tanto para seguir quanto para curtir fotos. Lembrando que o melhor jeito é fazer uma mistura dos 2 métodos para que o resultado seja mais variado, assim conseguindo seguidores locais e também seguidores de nicho. Seleções somente regionais tendem a conseguir menos seguidores, tendo em vista que são interações com pessoas aleatórias de tal região escolhida.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Quero somente seguidores locais, posso?'
+  	content: 'É possível, porém o melhor método é mesclar entre seguidores regionais e seguidores de nicho. Regiões específicas normalmente não têm a quantidade diária suficiente de fotos postadas e seguidores para alimentar nossa ferramenta de modo que todas interações sejam feitas, por isso a mescla é sempre o melhor jeito mesmo porque seguidores por nicho normalmente tem uma resposta maior à interação, ou seja, curtem suas fotos e seguem sua conta de volta mais facilmente.<br/>Cidades pequenas com menos de 500.000 habitantes é possível focar no público regional, porém será necessário focar as interações por nicho pois tais cidades não conseguem alimentar o nosso serviço, o que fará converter muito menos do que a média.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Moro no interior, vou conseguir seguidores locais?'
+  	content: 'Cidades muito pequenas conseguem seguidores locais também, porém como nosso serviço se limita a pessoas que tem conta de Instagram, cidades isoladas com menos de 100.000 habitantes as pessoas com contas no Instagram se esgotam muito mais rápido.<br/><br/>Caso sua cidade tenha cidades vizinhas próximas, conseguimos focar nas duas cidades e dar mais longevidade à captação de seguidores locais, porém a melhor maneira para tal caso é mesclar o foco com o foco no seu ramo também.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Vocês cobram taxas de configuração?'
+  	content: 'Não'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'Quanta tempo demora para configurar?'
+  	content: 'Configuramos a maioria das contas dentro de 12 horas úteis, porém esse período pode chegar até 24 horas em períodos de alta demanda. <br/>Para evitar qualquer tipo de bloqueio e expor sua conta a riscos, começamos o serviço apenas curtindo fotos e deixando de seguir quem não te segue no primeiro dia, no segundo dia começamos a seguir e no terceiro dia começamos as mensagens de boas vindas.<br/>Durante a fase de amaciamento, os valores são baixos, e vamos aumentando a cada semana até o máximo seguro para conseguirmos a maior quantidade de seguidores sem comprometermos sua conta.'
   },
   {
-  	title: ''
-  	content: ''
+  	title: 'E se eu atrasar a mensalidade?'
+  	content: 'O serviço será interrompido no dia do vencimento é será feita tentativas de contato para o retorno das atividades. Para que não ocorra pausa no serviço, pague sempre em dia.'
   },
 
 ]
