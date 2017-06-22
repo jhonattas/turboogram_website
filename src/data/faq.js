541b8cafd744e76706a16ed01d57028d78af200c
Jhonattas Henrique <jhonattas@soucriador.com>
2017-06-22 06:08:43 -0300

    adiciona um folder para dados sensiveis da aplicacao

---
+++ src/data/faq.js
@@ -0,0 +1,76 @@
+module.exports =
+[
+  {
+    title: 'Como funciona o serviço?'
+    content: 'Basicamente nós automatizamos a sua conta para seguir e curtir as fotos de seu público alvo com a sua conta do Instagram. Conseguimos focar as interações regionalmente e por hashtags comuns do seu ramo. Nossa ferramenta também faz o serviço de deixar de seguir as pessoas que não nos seguirem de volta, para evitar termos muitas pessoas sendo seguidas.'
+  },
+  {
+  	title: 'Quais os métodos de pagamento?'
+  	content: 'Aceitamos cartões e boletos, tudo feito pela PagSeguro ou Paypal para manter em segurança todos os dados.'
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+  {
+  	title: ''
+  	content: ''
+  },
+
+]
